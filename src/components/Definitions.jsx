4e38c0b24369e56fdcf16a9da8c5cfa91e624e25
Ivan Sharapov <oldrum75@gmail.com>
2024-09-15 15:35:55 +0600

    WIP: definition v2

--- src/components/Definitions.jsx
+++ src/components/Definitions.jsx
@@ -1,29 +1,38 @@
 const Definitions = ({ word, category, meanings }) => {
   return (
-    <div className="meanings flex flex-col overflow-y-scroll bg-green-500">
+    <div className="meanings flex flex-col overflow-y-scroll max-h-[70%] min-h-0 border-solid px-1 overflow-x-hidden border-2 border-neutral-700">
       {word === "" ? (
         <span className="subTitle">Start by typing a word in Search</span>
       ) : (
-        meanings.map((mean) => 
-          mean.meanings.map((item) =>
-            item.definitions.map((def) => (
-              <div
-                className="singleMean"
-                key={def.definition}
-                style={{ backgroundColor: "white", color: "black" }}
-              >
-                <b>{def.definition}</b>
-                <hr style={{ backgroundColor: "black", width: "100%" }} />
-                {def.example && (
-                  <span>
-                    <b>Example : </b>
-                    {def.example}
-                  </span>
-                )}
-              </div>
-            ))
-          )
-        )
+        meanings.map((mean) => {
+          return mean.meanings.map((item) => {
+            return item.definitions.map((def) => {
+              console.log("def", def);
+              return (
+                <div
+                  className="singleMean py-1"
+                  key={def.definition}
+                  style={{ backgroundColor: "white", color: "black" }}
+                >
+                  <b>{def.definition}</b>
+                  <hr style={{ backgroundColor: "black", width: "100%" }} />
+                  {def.example && (
+                    <span>
+                      <b>Example : </b>
+                      {def.example}
+                    </span>
+                  )}
+                  {def.synonyms.length !== 0 && (
+                    <span>
+                      <b>Synonyms : </b>
+                      {def.synonyms.map((s) => `${s}, `)}
+                    </span>
+                  )}
+                </div>
+              );
+            });
+          });
+        })
       )}
     </div>
   );
